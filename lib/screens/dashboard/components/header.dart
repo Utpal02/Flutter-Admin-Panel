6bb656941d5f2bec8f777e5cf51f5a83b79dd243
Utpal Tiwari <utpaltiwari02@gmail.com>
2024-02-12 21:48:27 +0530

    initial

--- lib/screens/dashboard/components/header.dart
+++ lib/screens/dashboard/components/header.dart
@@ -55,14 +55,14 @@ class ProfileCard extends StatelessWidget {
       child: Row(
         children: [
           Image.asset(
-            "assets/images/profile_pic.png",
+            "assets/images/profile_pic.jpg",
             height: 38,
           ),
           if (!Responsive.isMobile(context))
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-              child: Text("Angelina Jolie"),
+              child: Text("Utpal Tiwari"),
             ),
           Icon(Icons.keyboard_arrow_down),
         ],
